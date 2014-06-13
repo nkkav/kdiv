2bba3f6d2f599e60ea9a01c43281c516962756b7
U-uop-PC\uop <nikolaos.kavvadias@gmail.com>
2014-06-13 11:36:28 +0300

    Changed README to README.rst

--- rst2docs.sh
+++ rst2docs.sh
@@ -5,8 +5,8 @@
 #DOCUTILS_PATH="/usr/local/bin/docutils/tools"
 DOCUTILS_PATH="/c/docutils-0.11/tools"
 
-${DOCUTILS_PATH}/rst2html.py <$1 >$1.html
-${DOCUTILS_PATH}/rst2latex.py <$1 >$1.tex
+${DOCUTILS_PATH}/rst2html.py <$1.rst >$1.html
+${DOCUTILS_PATH}/rst2latex.py <$1.rst >$1.tex
 pdflatex --shell-escape $1.tex
 rm -rf $1.aux $1.log $1.out $1.tex
 
